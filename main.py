132e0f7ef48f43e70e6c8812e2ef5b53345b7be7
bjoernk1 <123405983+bjoernk1@users.noreply.github.com>
2023-06-24 21:02:49 +0200

    Aktualisiere main.blocks, main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-Index = 0
+x = 0
+y = 0
 
 def on_every_interval():
-    global Index
-    led.plot(Index, 0)
-    Index += -1
-    led.unplot(Index, 0)
-    Index += 2
-loops.every_interval(500, on_every_interval)
+    global x, y
+    led.plot(x, y)
+    x += -1
+    led.unplot(x, y)
+    x += 2
+    if x == 6:
+        x = 0
+        y += 1
+    if y == 6:
+        x = 0
+        y = 0
+loops.every_interval(100, on_every_interval)
