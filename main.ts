a50afd60e13274707aa83660db833c7fc5e84a4f
bjoernk1 <123405983+bjoernk1@users.noreply.github.com>
2023-06-26 20:54:13 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
-let x = 0
+let x = -1
 let y = 0
-loops.everyInterval(100, function () {
+let oldx = -1
+let oldy = -1
+function f1 () {
     led.plot(x, y)
     x += -1
     led.unplot(x, y)
@@ -13,4 +15,23 @@ loops.everyInterval(100, function () {
         x = 0
         y = 0
     }
+}
+
+function f2() {
+    oldx = x
+    oldy = y
+    x += 1
+    led.plot(x, y)
+    led.unplot(oldx, oldy)
+    if (x == 5) {
+        x = -1
+        y += 1
+    }
+    if (y == 5) {
+        x = -1
+        y = 0
+    }
+}
+loops.everyInterval(100, function () {
+    f2()
 })
